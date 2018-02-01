c862c962d3f65f7694069b79d17b556c788be786
MoaizAhmed <MoaizAhmed@hotmail.com>
2018-02-01 12:06:44 -0500

    Finished Part 4
    Index and read were changed in order to search for user thru location

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
-<!doctype html>
-<html lang="en">
+<?php include "templates/header.php"; ?>
 
+<ul>
+	<li><a href="create.php"><strong>Create</strong></a> - add a user</li>
+	<li><a href="read.php"><strong>Read</strong></a> - find a user</li>
+	<li><a href="install.php"><strong>Install</strong></a> - begin the database</li>
+</ul>
 
-	<?php include "templates/header.php"; ?>
-	<ul>
-		<li><a href="create.php"><strong>Create</strong></a> - add a user</li>
-		<li><a href="read.php"><strong>Read</strong></a> - find a user</li>
-	</ul>
-	<?php include "templates/footer.php"; ?>
-
+<?php include "templates/footer.php"; ?>
--- read.php
+++ read.php
@@ -1,4 +1,86 @@
-<?php include "templates/header.php"; ?>
+<?php
+
+/**
+ * Function to query information based on 
+ * a parameter: in this case, location.
+ *
+ */
+
+if (isset($_POST['submit'])) 
+{
+	
+	try 
+	{	
+		require "config.php";
+		require "common.php";
+
+		$connection = new PDO($dsn, $username, $password, $options);
+
+		$sql = "SELECT * 
+						FROM users
+						WHERE location = :location";
+
+		$location = $_POST['location'];
+
+		$statement = $connection->prepare($sql);
+		$statement->bindParam(':location', $location, PDO::PARAM_STR);
+		$statement->execute();
+
+		$result = $statement->fetchAll();
+	}
+	
+	catch(PDOException $error) 
+	{
+		echo $sql . "<br>" . $error->getMessage();
+	}
+}
+?>
+<?php require "templates/header.php"; ?>
+		
+<?php  
+if (isset($_POST['submit'])) 
+{
+	if ($result && $statement->rowCount() > 0) 
+	{ ?>
+		<h2>Results</h2>
+
+		<table>
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>First Name</th>
+					<th>Last Name</th>
+					<th>Email Address</th>
+					<th>Age</th>
+					<th>Location</th>
+					<th>Date</th>
+				</tr>
+			</thead>
+			<tbody>
+	<?php 
+		foreach ($result as $row) 
+		{ ?>
+			<tr>
+				<td><?php echo escape($row["id"]); ?></td>
+				<td><?php echo escape($row["firstname"]); ?></td>
+				<td><?php echo escape($row["lastname"]); ?></td>
+				<td><?php echo escape($row["email"]); ?></td>
+				<td><?php echo escape($row["age"]); ?></td>
+				<td><?php echo escape($row["location"]); ?></td>
+				<td><?php echo escape($row["date"]); ?> </td>
+			</tr>
+		<?php 
+		} ?>
+		</tbody>
+	</table>
+	<?php 
+	} 
+	else 
+	{ ?>
+		<blockquote>No results found for <?php echo escape($_POST['location']); ?>.</blockquote>
+	<?php
+	} 
+}?> 
 
 <h2>Find user based on location</h2>
 
@@ -10,4 +92,4 @@
 
 <a href="index.php">Back to home</a>
 
-<?php include "templates/footer.php"; ?>
+<?php require "templates/footer.php"; ?>
