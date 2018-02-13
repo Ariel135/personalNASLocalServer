e9e780c294601b3eab4373e14a25d22ee1ff8e39
MoaizAhmed <MoaizAhmed@hotmail.com>
2018-02-13 15:08:04 -0500

    BLOB Type Loading App
    Work with images

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<li><a href="create.php"><strong>Create</strong></a> - add a user</li>
 	<li><a href="read.php"><strong>Read</strong></a> - find a user</li>
 	<li><a href="install.php"><strong>Install</strong></a> - begin the database</li>
+	<li><a href="blobdemo.php"><strong>BLOBtut</strong></a> - files uploaded</li>
 </ul>
 
 <?php include "templates/footer.php"; ?>
